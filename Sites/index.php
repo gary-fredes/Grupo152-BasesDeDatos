1c8577a8ec160e3ec30a963f68c0064b94281d48
garyfredes <gary.fredes@uc.cl>
2021-05-24 17:51:32 -0400

    php

--- Sites/index.php
+++ Sites/index.php
@@ -18,10 +18,24 @@
 
   <h3 align="center"> Vehiculos de las unidades en la comuna</h3>
 
+  <?php
+  require("config/conexion.php");
+  $result = $db -> prepare("SELECT DISTINCT nombre FROM Comunas;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+
   <form align="center" action="consultas/consulta_vehiculos_unidades_en_comuna.php" method="post">
-    Comuna:
-    <input type="text" name="comuna">
-    <br/><br/>
+    Seleccinar una comuna:
+    <select name="comuna">
+      <?php
+      
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    <br><br>
     <input type="submit" value="Buscar">
   </form>
   
